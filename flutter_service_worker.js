d2fd191eaa04a7f0dc4f07f72aba839d17171dc8
unknown <kvenkateshk2004@gmailcom>
2026-03-25 13:43:59 +0530

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b435884878e35f76afe85a534c332e53
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "6fb8391042762022142f8c2f7479dfb3",
+"flutter_bootstrap.js": "e9aa4948f3e48b0cc2b0558f4dc33268",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
